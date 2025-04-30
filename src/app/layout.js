63a186e432fec7c0cb973e38caf3041aa6124830
IsmailofficialGithub <ismail.official295@gmail.com>
2025-04-30 13:18:43 +0530

    wellcome

---
+++ src/app/layout.js
@@ -0,0 +1,51 @@
+
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "BachatBzr",
+  description: "The cheapest second hand marketplace in Pakistan",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
+  openGraph: {
+    title: "BachatBzr",
+    description: "The cheapest second hand marketplace in Pakistan",
+    url: "https://bachatbzr.com",
+    siteName: "BachatBzr",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
